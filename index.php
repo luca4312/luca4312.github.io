fdf56070169878684fed9a198398ed471453a3f8
luca4312 <104347535+luca4312@users.noreply.github.com>
2022-12-07 15:50:35 +0100

    Add files via upload
    added beginning

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portofolio</title>
+    <style>href"style.css'</style>
+</head>
+<body>
+    <header>
+        <h1>Main page heading here</h1>
+        <p>Posted by John Doe</p>
+    </header>
+</body>
+</html>
