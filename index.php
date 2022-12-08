fa7093f7a913ba523783dfdd61285c787fcf6605
luca4312 <104347535+luca4312@users.noreply.github.com>
2022-12-08 09:09:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="nl">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>portofolio</title>
-    <style>href"style.css'</style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
-        <h1>Main page heading here</h1>
-        <p>Posted by John Doe</p>
+        <a href="Home.php">Home</a>
+        <a href="Catan.php">catan</a>
+        <a href="urenregistratiesysteem.php">URS</a>
     </header>
 </body>
 </html>
